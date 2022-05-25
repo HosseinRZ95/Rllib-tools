1d57dd6cdad2e6b77c9665330f49ebc3844988af
Hossein <80890032+HosseinRZ95@users.noreply.github.com>
2022-05-25 14:22:23 +0430

    Update and rename gaussian_noise to gaussian_noise.py

---
+++ action_distribution/gaussian_noise.py
@@ -0,0 +1,52 @@
+class TorchGaussian1(TorchDistributionWrapper):
+    """Based on Rllib normal distribution
+    It's simply like deterministic distribution with adding some random noise
+    you can change noise std in __init__"""
+
+    @override(ActionDistribution)
+    def __init__(
+        self,
+        inputs: List[TensorType],
+        model: TorchModelV2,
+        *,
+        action_space: Optional[gym.spaces.Space] = None
+    ):
+        super().__init__(inputs, model)
+        mean = (self.inputs)
+        print('mean',mean)
+        log_std = 0.0006
+        self.log_std = log_std
+        self.dist = torch.distributions.normal.Normal(mean,log_std)
+        # Remember to squeeze action samples in case action space is Box(shape)
+        self.zero_action_dim = action_space and action_space.shape == ()
+
+    @override(TorchDistributionWrapper)
+    def sample(self) -> TensorType:
+        sample = super().sample()
+         if self.zero_action_dim:
+             return torch.squeeze(sample, dim=-1)
+         print('sample',sample)
+         return sample
+
+    @override(ActionDistribution)
+    def deterministic_sample(self) -> TensorType:
+        self.last_sample = self.dist.mean
+        return self.last_sample
+
+    @override(TorchDistributionWrapper)
+    def logp(self, actions: TensorType) -> TensorType:
+        return super().logp(actions).sum(-1)
+
+    @override(TorchDistributionWrapper)
+    def entropy(self) -> TensorType:
+        return super().entropy().sum(-1)
+
+    @override(TorchDistributionWrapper)
+    def kl(self, other: ActionDistribution) -> TensorType:
+        return super().kl(other).sum(-1)
+
+    @override(ActionDistribution)
+    def required_model_output_shape(
+        action_space: gym.Space, model_config: ModelConfigDict
+    ) -> Union[int, np.ndarray]:
+        return np.prod(action_space.shape, dtype=np.int32) *1
